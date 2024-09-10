12b4330624ed5a0440ba7f4a50204274176d5281
wassim <wassim.mohammedi720@gmail.com>
2024-09-10 19:51:44 +0100

    connect html with sheet

--- hoco.js
+++ hoco.js
@@ -10,8 +10,7 @@ document.getElementById('submitButton').addEventListener('click', function() {
         phone: phone,
         selectedOption: selectedOptionText
     };
-    
-    // Send the data to the Google Apps Script URL
+
     fetch('https://script.google.com/macros/s/AKfycbykaOj_iyNO7aPMaC0mM9YJlF5FomCyI76WjDihhM2AKrTQvAjXqoi6UCXXbsIBcfSKfA/exec', {
         method: 'POST',
         mode: 'no-cors',
